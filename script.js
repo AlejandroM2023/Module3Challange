e96168f3de14ebfa3689ba152fde578c9122fefa
AlejandroM2023 <alejandromeza437@gmail.com>
2024-07-02 23:24:28 -0500

    output formatting

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const collectEmployees = function() {
     const firstName = prompt("What is the employees frist name.");
     const lastName = prompt("What is the employees last name.");
     const salary = prompt("What is the employees salary.");
-    const employee = {firstName: firstName,lastName:lastName,salary:salary};
+    const employee = {firstName: firstName,lastName:lastName,salary:Number(salary)};
     employeesArr.push(employee);
     addmore = confirm("Do you want to add more employees?");
   }
@@ -24,10 +24,11 @@ const displayAverageSalary = function(employeesArray) {
   // TODO: Calculate and display the average salary
   let avg=0;
   for(let i =0;i<employeesArray.length;i++){
-    avg = avg + Number(employeesArray[i].salary);
+    avg = avg + employeesArray[i].salary;
   }
-  avg = avg/employeesArray.length;
-  console.log(`The average employee salary between our ${employeesArray.length} employee(s) is ${avg}`);
+  avg = (avg/employeesArray.length).toFixed(2);
+  
+  console.log(`The average employee salary between our ${employeesArray.length} employee(s) is $${avg}`);
 }
 
 // Select a random employee
